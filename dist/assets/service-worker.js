464b2631cf741d76a01e4a66e2f652a1b4114622
Zak Keown <zak.keown@outlook.com>
2026-06-24 21:04:49 -0600

    Remove side panel assets and manifest files for the Throughline prototype

--- dist/assets/service-worker.js
+++
@@ -1,2 +0,0 @@
-(function(){"use strict";typeof chrome<"u"&&chrome.runtime.onInstalled.addListener(()=>{var e;(e=chrome.sidePanel)!=null&&e.setPanelBehavior&&chrome.sidePanel.setPanelBehavior({openPanelOnActionClick:!0})})})();
-//# sourceMappingURL=service-worker.js.map
